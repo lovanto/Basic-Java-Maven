9987fe8d938fed2f1bbe25fec7e4e25d2234ba57
Lovanto R <rifkylovanto@gmail.com>
2022-06-10 00:05:05 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -16,6 +16,34 @@ public class Driver {
 		
 		System.out.println("Current count:" + counter.getCount());
 		
+		counter.decrement();
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.decrement();
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.decrement();
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.decrement();
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.decrement();
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.decrement();
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.decrement();
+		
+		System.out.println("Current count:" + counter.getCount());
+		
 	}
 
 }
